720f974b9c33aef4e93a27a5d75662bbbb1c1591
k.korostylev <k.korostylev@gmail.com>
2023-03-02 13:39:15 +0500

    Animation_V1

--- app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
+++ app/src/main/java/ru/netology/nmedia/ui/AppActivity.kt
@@ -1,6 +1,15 @@
 package ru.netology.nmedia.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import ru.netology.nmedia.R
 
@@ -8,11 +17,17 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        findViewById<StatsView>(R.id.stats).data = listOf(
-            500F to 500F,
-            500F to 500F,
-            500F to 500F,
-            500F to 500F,
+        val view = findViewById<StatsView>(R.id.stats)
+        view.postDelayed(
+            {
+                view.data = listOf(
+                    500F to 500F,
+                    500F to 500F,
+                    500F to 500F,
+                    500F to 500F,
+                )
+            }, 10000
         )
     }
+
 }
--- app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
+++ app/src/main/java/ru/netology/nmedia/ui/StatsView.kt
@@ -1,5 +1,6 @@
 package ru.netology.nmedia.ui
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -7,7 +8,9 @@ import android.graphics.PointF
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.core.content.withStyledAttributes
+import kotlinx.coroutines.delay
 import ru.netology.nmedia.R
 import ru.netology.nmedia.util.AndroidUtils
 import kotlin.math.min
@@ -31,6 +34,9 @@ class StatsView @JvmOverloads constructor(
     private var lineWidth = AndroidUtils.dp(context, 5F).toFloat()
     private var fontSize = AndroidUtils.dp(context, 40F).toFloat()
     private var colors = emptyList<Int>()
+    private var progress = 0F
+    private var valueAnimator: ValueAnimator? = null
+
 
     init {
         context.withStyledAttributes(attrs, R.styleable.StatsView) {
@@ -59,6 +65,7 @@ class StatsView @JvmOverloads constructor(
     var data: List<Pair<Float, Float>> = emptyList()
         set(value) {
             field = value
+            update()
             //custom view перерисуется при условии видимости
             invalidate()
         }
@@ -103,14 +110,12 @@ class StatsView @JvmOverloads constructor(
             //каждому сектору случайный цвет
             paint.color = colors.getOrNull(index) ?: randomColor()
             //рисуем дугу
-            canvas.drawArc(oval, startFrom, angle, false, paint)
+            canvas.drawArc(oval, startFrom + 360 * progress, angle * progress, false, paint)
             startFrom += angle
             dotColor = colors.get(0)
-
-
         }
         canvas.drawText(
-            "%.2f%%".format(textPercent *100),
+            "%.2f%%".format(textPercent * 100 * progress),
             center.x,
             center.y + textPaint.textSize / 4,
             textPaint,
@@ -118,8 +123,27 @@ class StatsView @JvmOverloads constructor(
         //присваиваем цвет певого сектора для точки
         paint.color = dotColor
         //рисуем точку для корректировки верхней точки окружности
-        canvas.drawPoint(center.x,center.y - radius,paint)
-
+        //canvas.drawPoint(center.x,center.y - radius,paint)
     }
+
+    private fun update() {
+        valueAnimator?.let {
+            it.removeAllListeners()
+            it.cancel()
+        }
+        progress = 0F
+
+        valueAnimator = ValueAnimator.ofFloat(0F, 1F).apply {
+            addUpdateListener { anim ->
+                progress = anim.animatedValue as Float
+                invalidate()
+            }
+            duration = 500
+            interpolator = LinearInterpolator()
+        }.also {
+            it.start()
+        }
+    }
+
     private fun randomColor() = Random.nextInt(0xFF000000.toInt(), 0xFFFFFFFF.toInt())
 }
